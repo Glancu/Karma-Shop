20fdd735db63f23d614072c4e1a8236df6685bb0
Norbert Glanc <norglanc@gmail.com>
2020-08-12 12:17:20 +0200

    Add Newsletter entity, add API for newsletter

--- src/Controller/IndexApiController.php
+++ src/Controller/IndexApiController.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Entity\Contact;
+use App\Entity\Newsletter;
 use App\Form\Type\ContactType;
+use App\Form\Type\NewsletterType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -14,13 +16,13 @@ use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 /**
- * Class ContactController
+ * Class IndexApiController
  *
  * @package App\Controller
  *
- * @Route("/api/contact")
+ * @Route("/api")
  */
-final class ContactController {
+final class IndexApiController {
     private EntityManagerInterface $entityManager;
 
     private FormFactoryInterface $form;
@@ -31,16 +33,13 @@ final class ContactController {
      * @param EntityManagerInterface $entityManager
      * @param FormFactoryInterface $formFactory
      */
-    public function __construct(
-        EntityManagerInterface $entityManager,
-        FormFactoryInterface $formFactory)
-    {
+    public function __construct(EntityManagerInterface $entityManager, FormFactoryInterface $formFactory) {
         $this->entityManager = $entityManager;
         $this->form = $formFactory;
     }
 
     /**
-     * @Route("/create", name="add_contact", methods={"POST"})
+     * @Route("/contact/create", name="add_contact", methods={"POST"})
      *
      * @param Request $request
      * @param SerializerInterface $serializer
@@ -48,7 +47,7 @@ final class ContactController {
      *
      * @return JsonResponse
      */
-    public function request(Request $request, SerializerInterface $serializer, ValidatorInterface $validator): JsonResponse {
+    public function createContact(Request $request, SerializerInterface $serializer, ValidatorInterface $validator): JsonResponse {
         $em = $this->entityManager;
         $form = $this->form;
         $contact = new Contact();
@@ -65,7 +64,6 @@ final class ContactController {
 
         $contactForm = $form->create(ContactType::class, $contact);
         $contactForm->handleRequest($request);
-
         $contactForm->submit($data);
 
         $errors = $validator->validate($contact);
@@ -79,6 +77,52 @@ final class ContactController {
             $em->flush();
 
             $return = $serializer->serialize($contact, 'json');
+
+            return new JsonResponse($return);
+        }
+
+        return new JsonResponse((string)$errors);
+    }
+
+    /**
+     * @Route("/newsletter/create", name="add_newsletter", methods={"POST"})
+     *
+     * @param Request $request
+     * @param SerializerInterface $serializer
+     * @param ValidatorInterface $validator
+     *
+     * @return JsonResponse
+     */
+    public function createNewsletter(Request $request, SerializerInterface $serializer, ValidatorInterface $validator): JsonResponse {
+        $em = $this->entityManager;
+        $form = $this->form;
+        $newsletter = new Newsletter();
+
+        $name = $request->get('name') ?? '';
+        $email = $request->get('email');
+        $dataProcessingAgreement = (bool)$request->get('dataProcessingAgreement');
+
+        $data = [
+            'name' => $name,
+            'email' => $email,
+            'dataProcessingAgreement' => $dataProcessingAgreement
+        ];
+
+        $newsletterForm = $form->create(NewsletterType::class, $newsletter);
+        $newsletterForm->handleRequest($request);
+        $newsletterForm->submit($data);
+
+        $errors = $validator->validate($newsletter);
+
+        if($newsletterForm->isSubmitted() && $newsletterForm->isValid()) {
+            $newsletter->setName($name);
+            $newsletter->setEmail($email);
+            $newsletter->setDataProcessingAgreement($dataProcessingAgreement);
+
+            $em->persist($newsletter);
+            $em->flush();
+
+            $return = $serializer->serialize($newsletter, 'json');
 
             return new JsonResponse($return);
         }
---
+++ src/Form/Type/NewsletterType.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Form\Type;
+
+use App\Entity\Newsletter;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class NewsletterType extends AbstractType {
+    public function buildForm(FormBuilderInterface $builder, array $options): void {
+        $builder
+            ->add('name', TextType::class)
+            ->add('email', EmailType::class)
+            ->add('dataProcessingAgreement', CheckboxType::class)
+            ->add('save', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void {
+        $resolver->setDefaults([
+            'data_class' => Newsletter::class,
+            'csrf_protection' => false, // @TODO Remove this and implement JWT
+        ]);
+    }
+
+    public function getName(): string {
+        return 'newsletter';
+    }
+}
